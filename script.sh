33591d80fe4692ccd377400e3624a2937452dbe0
Atif Imam <Atif8Ted@users.noreply.github.com>
2017-11-21 15:54:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 cd /project
 python manage.py makemigrations
 python manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('ATIF', 'admin@example.com', 'password1234')" | python manage.py shell
 python manage.py runserver 0.0.0.0:8000
 
